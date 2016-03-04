939b58af547e9a304f77022a8ae2ec099ec3adf8
seabrae <seabrae@oclc.org>
2016-03-04 14:15:24 -0500

    d

---
+++ src/main/java/org/oclc/seek/flink/stream/job/KafkaToSolrJob.java
@@ -0,0 +1,99 @@
+/****************************************************************************************************************
+ *
+ *  Copyright (c) 2016 OCLC, Inc. All Rights Reserved.
+ *
+ *  OCLC proprietary information: the enclosed materials contain
+ *  proprietary information of OCLC, Inc. and shall not be disclosed in whole or in
+ *  any part to any third party or used by any person for any purpose, without written
+ *  consent of OCLC, Inc.  Duplication of any portion of these  materials shall include his notice.
+ *
+ ******************************************************************************************************************/
+package org.oclc.seek.flink.stream.job;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Properties;
+
+import org.apache.flink.api.common.accumulators.LongCounter;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.FileMonitoringFunction.WatchType;
+import org.oclc.seek.flink.job.JobContract;
+import org.oclc.seek.flink.job.JobGeneric;
+import org.oclc.seek.flink.stream.sink.KafkaSinkBuilder;
+
+/**
+ *
+ */
+public class KafkaToSolrJob extends JobGeneric implements JobContract {
+    private Properties props = new Properties();
+
+    @Override
+    public void init() {
+        String env = System.getProperty("environment");
+
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader) cl).getURLs();
+
+        for (URL url : urls) {
+            System.out.println(url.getFile());
+        }
+
+        String configFile = "conf/config." + env + ".properties";
+
+        // Properties properties = new Properties();
+        try {
+            props.load(ClassLoader.getSystemResourceAsStream(configFile));
+        } catch (Exception e) {
+            System.out.println("Failed to load the properties file... [" + configFile + "]");
+            e.printStackTrace();
+            throw new RuntimeException("Failed to load the properties file... [" + configFile + "]");
+        }
+
+        parameterTool = ParameterTool.fromMap(propertiesToMap(props));
+    }
+
+    @Override
+    public void execute() throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // make parameters available in the web interface
+        env.getConfig().setGlobalJobParameters(parameterTool);
+
+        // create a checkpoint every 5 secodns
+        // env.enableCheckpointing(5000);
+
+        // DataStream<String> text = env.readTextFile(parameterTool.getRequired("hdfs.kafka.source"));
+        DataStream<String> text =
+            env.readFileStream(parameterTool.getRequired("hdfs.solr.source"), 1000, WatchType.ONLY_NEW_FILES);
+
+        text.map(new RichMapFunction<String, String>() {
+            private static final long serialVersionUID = 1L;
+            private LongCounter recordCount = new LongCounter();
+
+            @Override
+            public void open(final Configuration parameters) throws Exception {
+                super.open(parameters);
+                getRuntimeContext().addAccumulator("recordCount", recordCount);
+            }
+
+            @Override
+            public String map(final String value) throws Exception {
+                recordCount.add(1L);
+                return value;
+            }
+        }).name("json-records")
+        .addSink(new KafkaSinkBuilder().build(parameterTool.get("kafka.topic"),
+            parameterTool.getProperties()))
+            .name("kafka");
+
+        // transformed.writeAsText("hdfs:///" + parameterTool.getRequired("hdfs.wordcount.output"));
+
+        env.execute("Reads from HDFS and writes to Kafka");
+    }
+
+}
