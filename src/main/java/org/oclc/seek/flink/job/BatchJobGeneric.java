e4f4c5a90494d548d1839b4a8375686c63be9841
seabrae <seabrae@oclc.org>
2016-03-04 15:47:35 -0500

    d

---
+++ src/main/java/org/oclc/seek/flink/job/BatchJobGeneric.java
@@ -0,0 +1,53 @@
+/****************************************************************************************************************
+ *
+ *  Copyright (c) 2016 OCLC, Inc. All Rights Reserved.
+ *
+ *  OCLC proprietary information: the enclosed materials contain
+ *  proprietary information of OCLC, Inc. and shall not be disclosed in whole or in
+ *  any part to any third party or used by any person for any purpose, without written
+ *  consent of OCLC, Inc.  Duplication of any portion of these  materials shall include his notice.
+ *
+ ******************************************************************************************************************/
+package org.oclc.seek.flink.job;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
+
+/**
+ *
+ */
+public abstract class BatchJobGeneric implements BatchJobContract {
+    protected ParameterTool parameterTool;
+
+    /**
+     * @param query
+     */
+    public void init(final String query) {
+    }
+
+    @Override
+    public void init() {
+        Properties props = new Properties();
+
+        parameterTool = ParameterTool.fromMap(propertiesToMap(props));
+    }
+
+    /**
+     * @param props
+     * @return an {@link Map} instance of a {@link Properties} object
+     */
+    public Map<String, String> propertiesToMap(final Properties props) {
+        Map<String, String> map = new HashMap<String, String>();
+        for (String key : props.stringPropertyNames()) {
+            map.put(key, props.getProperty(key));
+        }
+        return map;
+    }
+
+    @Override
+    public abstract void execute(ExecutionEnvironment env) throws Exception;
+}
